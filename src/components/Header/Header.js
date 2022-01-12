b125902844441020983f558725a1038fc41316cc
HectorAllende <hectorallende@gmail.com>
2022-01-12 19:01:48 -0300

    agregando hamburger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import React, { useState} from 'react';
 import Slider from './slider';
-
-
+import "./header.css"
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Header = () => {
 
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const toggleMenuClick = () => {
+        setMenuClicked(!menuClicked);
+    };
+
+
+
+
     const [navBar, setNavBar] = useState(false)
 
- 
+
     const handleClick = (e) => {
         e.preventDefault()
         const target = e.target.getAttribute('href')
@@ -19,10 +28,10 @@ const Header = () => {
         })
     }
 
-    const changeBackground = ()=>{
-        if(window.scrollY >=100){
+    const changeBackground = () => {
+        if (window.scrollY >= 100) {
             setNavBar(true)
-        }else{
+        } else {
             setNavBar(false)
         }
     }
@@ -39,13 +48,28 @@ const Header = () => {
                             <img src="img/logo.svg" alt="logo" />
                         </a>
                     </div>
-                    <div className='enlaces-header enlaces' id="enlaces">
-                        <a href="#" className={navBar ? 'enlace2' : 'enlace'}>Home</a>
-                        <a href="#about" className={navBar ? 'enlace2' : 'enlace'} onClick={handleClick} >About us</a>
-                        <a href="#services" className={navBar ? 'enlace2' : 'enlace'} onClick={handleClick}>Services</a>
-                        <a href="#products" className={navBar ? 'enlace2' : 'enlace'} onClick={handleClick}>Products</a>
-                        <a href="#details" className={navBar ? 'enlace2' : 'enlace'} onClick={handleClick}>Details</a>
-                        <a href="#contact" className={navBar ? 'enlace2' : 'enlace'} onClick={handleClick}>Contact</a>
+
+                    {menuClicked ? (
+                        <FiX size={25} className={"navbar__menu"} onClick={toggleMenuClick} />
+                    ) : (
+                        <FiMenu
+                            size={25}
+                            className={"navbar__menu"}
+                            onClick={toggleMenuClick}
+                        />
+                    )}
+
+
+                    <div className={
+          menuClicked ? "navbar__list navbar__list--active enlaces-header enlaces" : "navbar__list enlaces-header enlaces"
+        } id="enlaces">
+
+                        <a href="#" className={navBar ? 'enlace2 navbar__link ' : 'enlace navbar__link'}>Home</a>
+                        <a href="#about" className={navBar ? 'enlace2 navbar__link' : 'enlace navbar__link'} onClick={handleClick} >About us</a>
+                        <a href="#services" className={navBar ? 'enlace2 navbar__link ' : 'enlace navbar__link'} onClick={handleClick}>Services</a>
+                        <a href="#products" className={navBar ? 'enlace2 navbar__link' : 'enlace navbar__link'} onClick={handleClick}>Products</a>
+                        <a href="#details" className={navBar ? 'enlace2 navbar__link' : 'enlace navbar__link'} onClick={handleClick}>Details</a>
+                        <a href="#contact" className={navBar ? 'enlace2 navbar__link' : 'enlace navbar__link'} onClick={handleClick}>Contact</a>
                     </div>
                 </section>
             </nav>
